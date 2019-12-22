18c8c40000ee50dd6817a6f0757fffd13e48207d
Hung Ho <hungho@Hungs-MacBook-Pro.local>
2019-12-22 17:14:28 +0200

    Add test DNS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const http = require('http')
+const dns = require('dns')
 
 const app = express()
 const connectToDatabase = require('./db')
@@ -9,7 +10,7 @@ const Note = require('./Note')
 app.get('/', async (req, res) => {
     await connectToDatabase()
     const notes = await Note.find()
-    res.send({ v: 2, notes })
+    res.send({ v: 2, notes, dns: dns.getServers() })
 })
 
 http.createServer(app).listen(process.env.PORT || 8000)
